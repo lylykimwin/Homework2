60547bf4e48593ccec1f9f1da5c35472ccbbe224
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-09-15 22:10:03 -0500

    Update get.php

--- get.php
+++ get.php
@@ -1,9 +1,9 @@
 <?php
-$pageTitle = "Make a Reservation";
+$pageTitle = "Get Reservation";
 include "view-header.php";
 ?>
-<h1>Make a Reservation</h1>
-<form method="get" action="get-result.php">
+<h1>Reserve a Table</h1>
+<form action="get-results.php" method="get">
     <div class="mb-3">
         <label for="name" class="form-label">Name:</label>
         <input type="text" class="form-control" id="name" name="name" required>
@@ -20,4 +20,8 @@ include "view-header.php";
         <label for="guests" class="form-label">Number of Guests:</label>
         <input type="number" class="form-control" id="guests" name="guests" required>
     </div>
-  
+    <button type="submit" class="btn btn-primary">Reserve</button>
+</form>
+<?php
+include "view-footer.php";
+?>
