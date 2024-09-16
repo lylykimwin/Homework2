6fac3ffa78532c47c359ee841f4d83f83423700f
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-09-15 21:34:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<h1>Home</h1>
+<h1>Welcome to Lyly's Cafe</h1>
+<p>Discover our delicious menu, make a reservation, or share your feedback with us!</p>
 <?php
 include "view-footer.php";
 ?>
