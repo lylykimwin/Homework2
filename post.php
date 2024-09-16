f6d20c8e419fa22de616cd27bd57a4d9f8708610
lylykimwin <lyly.k.nguyen-1@ou.edu>
2024-09-15 21:38:44 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,11 +1,14 @@
 <?php
-$pageTitle = "Post";
+$pageTitle = "Submit Feedback";
 include "view-header.php";
 ?>
-<h1>Post</h1>
+<h1>Submit Your Feedback</h1>
 <form method="post" action="post-result.php">
- <input type="text" name="my-name">
- <input type="submit" value="Post data">
+    <div class="mb-3">
+        <label for="dish-suggestion" class="form-label">Dish Suggestion or Feedback:</label>
+        <input type="text" class="form-control" id="dish-suggestion" name="dish-suggestion" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 <?php
 include "view-footer.php";
